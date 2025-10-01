0c421da749e83751c15609d90b9af7d2c2e9ff86
MargheritaNava <e.nava001@studenti.unibs.it>
2025-10-01 11:09:19 +0200

    fix: Update startup scripts

--- bat/gazebo_bridge.bat
+++ bat/gazebo_bridge.bat
@@ -1,1 +1,2 @@
-powershell -NoProfile -ExecutionPolicy Bypass -File "C:\repos\DTblocklyGPT\bat\gazebo_bridge.ps1"
+@echo off
+wsl.exe -d Ubuntu-24.04 -e bash -c "source /opt/ros/jazzy/setup.bash && cd /mnt/c/Users/kinno/git/DTblocklyGPT/ros2_ws && source .venv/bin/activate && source install/setup.bash && ros2 run ros_gz_bridge parameter_bridge --ros-args -p config_file:=./Cobotta/map.yaml"
